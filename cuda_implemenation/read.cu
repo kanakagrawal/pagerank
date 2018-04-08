c3bafcd94c558ee29fc32b8e55c3106ce4716b24
ShraddheyaS <shendreshraddheya@gmail.com>
2018-04-08 16:00:37 +0530

    reading into sparse matrix done

---
+++ cuda_implemenation/read.cu
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <assert.h>
+#include <string>
+#include <fstream>
+
+#include "Utilities.cuh"
+
+#include <cuda_runtime.h>
+#include <cusparse_v2.h>
+
+using namespace std;
+
+void read(string filename, double** P_sparse, int** row_ind, int** col_ind, int* nnz, int * n)
+{
+	fstream f(filename.c_str());
+	int v, e;
+	f >> v;
+	f >> e;
+	*nnz = e;
+	*n = v;
+	string dummy;
+	int du;
+	for(int i = 0; i < v; i++)
+	{
+		f >> du >> dummy;
+	}
+	
+	*row_ind = new int[e];
+	*col_ind = new int[v + 1];
+	
+	for(int i = 0; i < v + 1; i++)
+	{
+		(*col_ind)[i] = 0;
+	}
+	
+
+	*P_sparse = new double[e];
+	
+	int curLength = 0;
+	int curRow, prevRow = 0;
+	for(int i = 0; i < *nnz; i++)
+	{
+		f >> (*row_ind)[i];
+		f >> curRow;
+		if (curRow != prevRow)
+		{
+			(*col_ind)[prevRow] = curLength;	
+			prevRow = curRow;
+		}
+		curLength++;
+		(*P_sparse)[i] = 1.0;
+	}
+}
+
+int main()
+{
+	string filename = "hollins.dat";
+	int *row_ind, *col_ind;
+	double* P_sparse;
+	int nnz, n;
+	read(filename, &P_sparse, &row_ind, &col_ind, &nnz, &n);
+	cout << n << endl << nnz << endl;
+	
+	cout << col_ind[0] << endl << col_ind[n-1] << endl;
+}
