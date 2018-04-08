1d1e934c384e26c2c2982fc368d6b4d7bb488b77
kanakagrawal <kanakagrawal15140@gmail.com>
2018-04-08 23:18:58 +0530

    Data generation modified

--- generate_data.py
+++ generate_data.py
@@ -9,16 +9,14 @@ V = int(sys.argv[1])
 E = int(sys.argv[2])
 filename = sys.argv[3]
 
-
-g = Graph()
-g.add_vertices(V)
-for i in range(E):
-	x,y = randint(0,V-1),randint(0,V-1)
-	g.add_edges([(x,y)])
-edges = g.get_edgelist()
 with open(filename,"w") as f:
 	f.write("%s %s\n"%(V,E))
 	for i in range(V):
+		if i %100000 == 0 :
+			print i
 		f.write("%s %s\n"%(i,"link"+str(i)))
 	for i in range(E):
-		f.write("%s %s\n"%(edges[i][0],edges[i][1]))
+		if i %100000 == 0 :
+			print i
+		x,y = randint(0,V-1),randint(0,V-1)
+		f.write("%s %s\n"%(x,y))
