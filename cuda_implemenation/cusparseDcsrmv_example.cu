32b4c6cd2b76022ca223b984ed6253528bd14ef3
ShraddheyaS <shendreshraddheya@gmail.com>
2018-04-08 17:04:03 +0530

    fixed

--- cuda_implemenation/cusparseDcsrmv_example.cu
+++ cuda_implemenation/cusparseDcsrmv_example.cu
@@ -2,11 +2,16 @@
 #include <stdlib.h>
 #include <iostream>
 #include <assert.h>
+#include <string>
+#include <fstream>
 
 #include "Utilities.cuh"
 
 #include <cuda_runtime.h>
 #include <cusparse_v2.h>
+using namespace std;
+
+void read(string filename, double** P_sparse, int** row_ind, int** col_ind, int* nnz, int * n, int** nnzPerVectorA);
 
 /********/
 /* MAIN */
@@ -19,87 +24,58 @@ int main()
     /**************************/
     /* SETTING UP THE PROBLEM */
     /**************************/
-    const int N     = 4;                // --- Number of rows and columns
-
-    // --- Host side dense matrices
-    double *h_A_dense = (double*)malloc(N * N * sizeof(double));
-    double *h_x_dense = (double*)malloc(N *     sizeof(double));
-    double *h_y_dense = (double*)malloc(N *     sizeof(double));
-
-    // --- Column-major ordering
-    h_A_dense[0] = 0.4612;  h_A_dense[4] = -0.0006;     h_A_dense[8]  = 0.3566;     h_A_dense[12] = 0.0; 
-    h_A_dense[1] = -0.0006; h_A_dense[5] = 0.4640;      h_A_dense[9]  = 0.0723;     h_A_dense[13] = 0.0; 
-    h_A_dense[2] = 0.3566;  h_A_dense[6] = 0.0723;      h_A_dense[10] = 0.7543;     h_A_dense[14] = 0.0; 
-    h_A_dense[3] = 0.;      h_A_dense[7] = 0.0;         h_A_dense[11] = 0.0;        h_A_dense[15] = 0.1; 
-
-    // --- Initializing the data and result vectors
-    for (int k = 0; k < N; k++) {
-        h_x_dense[k] = 1.;
-        h_y_dense[k] = 0.;
-    }
-
-    // --- Create device arrays and copy host arrays to them
-    double *d_A_dense;  gpuErrchk(cudaMalloc(&d_A_dense, N * N * sizeof(double)));
-    double *d_x_dense;  gpuErrchk(cudaMalloc(&d_x_dense, N     * sizeof(double)));
-    double *d_y_dense;  gpuErrchk(cudaMalloc(&d_y_dense, N     * sizeof(double)));
-    gpuErrchk(cudaMemcpy(d_A_dense, h_A_dense, N * N * sizeof(double), cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpy(d_x_dense, h_x_dense, N     * sizeof(double), cudaMemcpyHostToDevice));
-    gpuErrchk(cudaMemcpy(d_y_dense, h_y_dense, N     * sizeof(double), cudaMemcpyHostToDevice));
+    string filename = "data.dat";
+	int *row_ind, *col_ind, *nnzPerVectorA;
+	double* P_sparse;
+	int nnz, n;
+	read(filename, &P_sparse, &row_ind, &col_ind, &nnz, &n, &nnzPerVectorA);
+    const int N     = n;                // --- Number of rows and columns
 
     // --- Descriptor for sparse matrix A
     cusparseMatDescr_t descrA;      cusparseSafeCall(cusparseCreateMatDescr(&descrA));
     cusparseSafeCall(cusparseSetMatType     (descrA, CUSPARSE_MATRIX_TYPE_GENERAL));
     cusparseSafeCall(cusparseSetMatIndexBase(descrA, CUSPARSE_INDEX_BASE_ZERO));  
 
-    int nnzA = 0;                           // --- Number of nonzero elements in dense matrix A
+    int nnzA = nnz;                           // --- Number of nonzero elements in dense matrix A
 
     const int lda = N;                      // --- Leading dimension of dense matrix
 
-    // --- Device side number of nonzero elements per row of matrix A
-    int *d_nnzPerVectorA;   gpuErrchk(cudaMalloc(&d_nnzPerVectorA, N * sizeof(*d_nnzPerVectorA)));
-    cusparseSafeCall(cusparseDnnz(handle, CUSPARSE_DIRECTION_ROW, N, N, descrA, d_A_dense, lda, d_nnzPerVectorA, &nnzA));
-
-    // --- Host side number of nonzero elements per row of matrix A
-    int *h_nnzPerVectorA = (int *)malloc(N * sizeof(*h_nnzPerVectorA));
-    gpuErrchk(cudaMemcpy(h_nnzPerVectorA, d_nnzPerVectorA, N * sizeof(*h_nnzPerVectorA), cudaMemcpyDeviceToHost));
-
     printf("Number of nonzero elements in dense matrix A = %i\n\n", nnzA);
-    for (int i = 0; i < N; ++i) printf("Number of nonzero elements in row %i for matrix = %i \n", i, h_nnzPerVectorA[i]);
+    for (int i = 0; i < N; ++i) printf("Number of nonzero elements in row %i for matrix = %i \n", i, nnzPerVectorA[i]);
     printf("\n");
-
-    // --- Device side sparse matrix
-    double *d_A;            gpuErrchk(cudaMalloc(&d_A, nnzA * sizeof(*d_A)));
-
-    int *d_A_RowIndices;    gpuErrchk(cudaMalloc(&d_A_RowIndices, (N + 1) * sizeof(*d_A_RowIndices)));
-    int *d_A_ColIndices;    gpuErrchk(cudaMalloc(&d_A_ColIndices, nnzA * sizeof(*d_A_ColIndices)));
-
-    cusparseSafeCall(cusparseDdense2csr(handle, N, N, descrA, d_A_dense, lda, d_nnzPerVectorA, d_A, d_A_RowIndices, d_A_ColIndices));
-
-    // --- Host side sparse matrices
-    double *h_A = (double *)malloc(nnzA * sizeof(*h_A));        
-    int *h_A_RowIndices = (int *)malloc((N + 1) * sizeof(*h_A_RowIndices));
-    int *h_A_ColIndices = (int *)malloc(nnzA * sizeof(*h_A_ColIndices));
-    gpuErrchk(cudaMemcpy(h_A, d_A, nnzA * sizeof(*h_A), cudaMemcpyDeviceToHost));
-    gpuErrchk(cudaMemcpy(h_A_RowIndices, d_A_RowIndices, (N + 1) * sizeof(*h_A_RowIndices), cudaMemcpyDeviceToHost));
-    gpuErrchk(cudaMemcpy(h_A_ColIndices, d_A_ColIndices, nnzA * sizeof(*h_A_ColIndices), cudaMemcpyDeviceToHost));
 
     printf("\nOriginal matrix A in CSR format\n\n");
-    for (int i = 0; i < nnzA; ++i) printf("A[%i] = %f ", i, h_A[i]); printf("\n");
+    for (int i = 0; i < nnzA; ++i) printf("A[%i] = %f ", i, P_sparse[i]); printf("\n");
 
     printf("\n");
-    for (int i = 0; i < (N + 1); ++i) printf("h_A_RowIndices[%i] = %i \n", i, h_A_RowIndices[i]); printf("\n");
+    for (int i = 0; i < (N + 1); ++i) printf("P_sparse_RowIndices[%i] = %i \n", i, col_ind[i]); printf("\n");
 
     printf("\n");
-    for (int i = 0; i < nnzA; ++i) printf("h_A_ColIndices[%i] = %i \n", i, h_A_ColIndices[i]);  
+    for (int i = 0; i < nnzA; ++i) printf("P_sparse_ColIndices[%i] = %i \n", i, row_ind[i]);  
 
+	double *h_x_dense = (double*)malloc(N * sizeof(double));
+	double *h_y_dense = (double*)malloc(N * sizeof(double));
+	double *d_A; gpuErrchk(cudaMalloc(&d_A, nnzA * sizeof(*d_A)));
+	for (int k = 0; k < N; k++)
+	{
+        h_x_dense[k] = 1.;
+        h_y_dense[k] = 0.;
+	}
+	double *d_x_dense;  gpuErrchk(cudaMalloc(&d_x_dense, N     * sizeof(double)));
+	double *d_y_dense; gpuErrchk(cudaMalloc(&d_y_dense, N * sizeof(double)));
+	gpuErrchk(cudaMemcpy(d_x_dense, h_x_dense, N     * sizeof(double), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_y_dense, h_y_dense, N * sizeof(double), cudaMemcpyHostToDevice));
+	gpuErrchk(cudaMemcpy(d_A, P_sparse, nnzA * sizeof(*P_sparse), cudaMemcpyHostToDevice));
+	
     printf("\n");
     for (int i = 0; i < N; ++i) printf("h_x[%i] = %f \n", i, h_x_dense[i]); printf("\n");
 
     const double alpha = 1.;
     const double beta  = 0.;
-    cusparseSafeCall(cusparseDcsrmv(handle, CUSPARSE_OPERATION_NON_TRANSPOSE, N, N, nnzA, &alpha, descrA, d_A, d_A_RowIndices, d_A_ColIndices, d_x_dense, 
+    cusparseSafeCall(cusparseDcsrmv(handle, CUSPARSE_OPERATION_TRANSPOSE, N, N, nnzA, &alpha, descrA, d_A, col_ind, row_ind, d_x_dense, 
                                     &beta, d_y_dense));
-
+	cudaDeviceSynchronize();                                 
+	cout << "I am here" << endl;
     gpuErrchk(cudaMemcpy(h_y_dense,           d_y_dense,            N * sizeof(double), cudaMemcpyDeviceToHost));
 
     printf("\nResult vector\n\n");
--- cuda_implemenation/read.cu
+++ cuda_implemenation/read.cu
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void read(string filename, double** P_sparse, int** row_ind, int** col_ind, int* nnz, int * n)
+void read(string filename, double** P_sparse, int** row_ind, int** col_ind, int* nnz, int * n, int** nnzPerVectorA)
 {
 	fstream f(filename.c_str());
 	int v, e;
@@ -21,6 +21,7 @@ void read(string filename, double** P_sparse, int** row_ind, int** col_ind, int*
 	
 	*row_ind = new int[e];
 	*col_ind = new int[v + 1];
+	*nnzPerVectorA = new int[v];
 	
 	for(int i = 0; i < v + 1; i++)
 	{
@@ -30,30 +31,39 @@ void read(string filename, double** P_sparse, int** row_ind, int** col_ind, int*
 
 	*P_sparse = new double[e];
 	
+	int curLengthCumulative = 0;
 	int curLength = 0;
 	int curRow, prevRow = 0;
 	for(int i = 0; i < *nnz; i++)
 	{
 		f >> (*row_ind)[i];
+		(*row_ind)[i]--;
 		f >> curRow;
+//		curRow--;
 		if (curRow != prevRow)
 		{
-			(*col_ind)[prevRow] = curLength;	
+			(*col_ind)[prevRow] = curLengthCumulative;	
+			(*nnzPerVectorA)[prevRow - 1] = curLength;
+			curLength = 0;
 			prevRow = curRow;
 		}
 		curLength++;
+		curLengthCumulative++;
 		(*P_sparse)[i] = 1.0;
 	}
+	(*col_ind)[curRow] = curLengthCumulative;
+	(*nnzPerVectorA)[prevRow - 1] = curLength;
 }
-
+/*
 int main()
 {
 	string filename = "hollins.dat";
-	int *row_ind, *col_ind;
+	int *row_ind, *col_ind, *nnzPerVectorA;
 	double* P_sparse;
 	int nnz, n;
-	read(filename, &P_sparse, &row_ind, &col_ind, &nnz, &n);
+	read(filename, &P_sparse, &row_ind, &col_ind, &nnz, &n, &nnzPerVectorA);
 	cout << n << endl << nnz << endl;
 	
 	cout << col_ind[0] << endl << col_ind[n-1] << endl;
 }
+*/
