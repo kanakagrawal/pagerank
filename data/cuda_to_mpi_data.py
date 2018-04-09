97ee160f30eede26ddb6bb7e71558079e8a631e0
kanakagrawal <kanakagrawal15140@gmail.com>
2018-04-09 19:52:25 +0530

    Data from CUDA to mpi

---
+++ data/cuda_to_mpi_data.py
@@ -0,0 +1,28 @@
+import sys
+
+filename = sys.argv[1]
+adj_list = []
+with open(filename,"r") as f:
+	x= f.readline()
+	V,E,temp = x.split()
+	V = int(V)
+	E = int(E)
+	temp = int(temp)
+	for i in range(V):
+		adj_list.append([])
+	if temp == 1:
+		for i in range(V):
+			x = f.readline()
+	for i in range(E):
+		x = f.readline()
+		a,b = x.split()
+		a = int(a)
+		b = int(b)
+		adj_list[a-1].append(b-1)
+for i in range(V):
+	adj_list[i].sort()
+with open("mpi_"+filename,"w") as f:
+	for i in range(V):
+		x = ' '.join(map(str, adj_list[i]))
+		out = str(i) + " " + x + "\n"
+		f.write(out)
