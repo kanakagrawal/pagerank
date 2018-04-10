563af9626fb583bd5130d72a78548b1819831de9
kanakagrawal <kanakagrawal15140@gmail.com>
2018-04-11 04:26:58 +0530

    Final

--- cuda_implemenation/sort.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import sys
-import numpy as np
-
-with open (sys.argv[1]) as f:
-    arr = np.array([ np.float(line.split(sep=' ')[1]) for line in f.readlines()])
-    print (arr)
-    print (np.argsort(arr))
