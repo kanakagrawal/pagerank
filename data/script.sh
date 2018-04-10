836c7aef4892b552114722c6c036972e9c49bd86
kanakagrawal <kanakagrawal15140@gmail.com>
2018-04-10 17:04:37 +0530

    Minor change in script

--- data/script.sh
+++ data/script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for f in ../mpj/PageRankMPI/Inputfile/*; do
+for f in ../mpj/PageRankMPI/Inputfile/pagerank*; do
 	b=$(basename $f)
  	echo "File -> $b"
  	python3 mpi_to_cuda_data.py $f
